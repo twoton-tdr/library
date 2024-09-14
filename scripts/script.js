158ca6b39da1cf2cf84a735273b96569209b9d17
twoton tdr <79171113+twoton-tdr@users.noreply.github.com>
2024-09-14 22:20:40 +0530

    added dialog

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+let myLibrary = [];
+
+function Book(name, author, genre) {
+    this.name = name;
+    this.author = author;
+    this.genre = genre;
+}
+
+const dialog = document.querySelector("dialog");
+
+
+const addButton = document.querySelector(".sidebar-add");
+addButton.addEventListener("click", ()=>{dialog.showModal();});
+
+const confirmButton = document.querySelector("dialog button");
+confirmButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let bookName = document.querySelector("#book_name");
+    bookName = bookName.value;
+
+    let authorName = document.querySelector("#author_name");
+    authorName = authorName.value;
+
+    let genre = document.querySelector("#genre_n");
+    genre = genre.value;
+
+    window[bookName] = new Book(bookName, authorName, genre); //creating global variable containing content inside the bookName
+
+
+    console.log(window[bookName])
+
+    myLibrary.push(window[bookName]);
+    dialog.close();
+    document.querySelector("dialog form").reset();
+    console.log(myLibrary);
+})
+
+
